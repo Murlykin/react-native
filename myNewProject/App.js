f3ea572cb68d10c01ae9e1f1392294b8eac6a1af
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-03-18 19:27:14 +0200

    3

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/")}
+      ></ImageBackground>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -17,4 +23,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+      },
 });
